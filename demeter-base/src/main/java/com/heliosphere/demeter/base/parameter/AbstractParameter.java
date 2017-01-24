cbedc88aebec0e24135a4017e8f11858677fde38
Resse Christophe <christophe.resse@gmail.com>
2017-01-24 17:12:00 +0100

    Added the AbstractParameter class.

---
+++ demeter-base/src/main/java/com/heliosphere/demeter/base/parameter/AbstractParameter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright(c) 2016 - Heliosphere Corp.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Heliosphere's project which is licensed under the 
+ * Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.heliosphere.demeter.base.parameter;
+
+/**
+ * Provides an abstract implementation of a parameter.
+ * <hr>
+ * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Heliosphere</a>
+ * @version 1.0.0
+ */
+public class AbstractParameter implements IParameter
+{
+	/**
+	 * Default serialization identifier.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Parameter name.
+	 */
+	private String name;
+
+	/**
+	 * Parameter type.
+	 */
+	private Enum<? extends IParameterType> type;
+
+	@Override
+	public final String getName()
+	{
+		return name;
+	}
+
+	@Override
+	public final void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
+	public final Enum<? extends IParameterType> getType()
+	{
+		return type;
+	}
+
+	@Override
+	public final void setType(Enum<? extends IParameterType> type)
+	{
+		this.type = type;
+	}
+
+	@Override
+	public int compareTo(IParameter o)
+	{
+		return name.compareTo(o.getName());
+	}
+}
