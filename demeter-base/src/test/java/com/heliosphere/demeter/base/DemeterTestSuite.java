0604536fc081bb522ff39e614e1ab4c1319a9264
Resse Christophe <christophe.resse@gmail.com>
2017-01-31 18:52:06 +0100

    Fixed the test suite for demeter module.

--- demeter-base/src/test/java/com/heliosphere/demeter/base/DemeterTestSuite.java
+++ demeter-base/src/test/java/com/heliosphere/demeter/base/DemeterTestSuite.java
@@ -11,14 +11,12 @@
  */
 package com.heliosphere.demeter.base;
 
-import org.junit.experimental.categories.Categories;
-import org.junit.experimental.categories.Categories.IncludeCategory;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.heliosphere.demeter.base.file.xml.test.XmlRunnerParameterFileTest;
 import com.heliosphere.demeter.base.file.xml.test.XmlFileTest;
-import com.heliosphere.demeter.base.log4j.category.UnitTest;
+import com.heliosphere.demeter.base.file.xml.test.XmlRunnerParameterFileTest;
 import com.heliosphere.demeter.base.resource.test.ResourceTest;
 
 /**
@@ -27,8 +25,7 @@ import com.heliosphere.demeter.base.resource.test.ResourceTest;
  * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Heliosphere</a>
  * @version 1.0.0
  */
-@RunWith(Categories.class)
-@IncludeCategory(UnitTest.class)
+@RunWith(Suite.class)
 @SuiteClasses({ ResourceTest.class, XmlFileTest.class, XmlRunnerParameterFileTest.class })
 public class DemeterTestSuite
 {
